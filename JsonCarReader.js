a34a85513a758b48e82e189fd5aeea4840a58db0
Leander-Haddal <122444622+Leander-Haddal@users.noreply.github.com>
2024-03-07 09:42:49 +0100

    Json reader to change search.html cards

---
+++ JsonCarReader.js
@@ -0,0 +1,59 @@
+fetch('Cars.json')
+    .then(response => response.json())
+    .then(data => {
+        const cardsContainer = document.getElementById('cards');
+        cardsContainer.innerHTML = '';
+        data.cars.forEach(car => createCard(car));
+    });
+
+
+    // main method
+function createCard(car) {
+    
+    const card = document.createElement('div');
+    card.className = 'card';
+
+  
+    const img = createImageElement(car);
+    card.appendChild(img);
+
+    
+    const cardInfo = createCardInfoElement(car);
+    card.appendChild(cardInfo);
+
+    document.getElementById('cards').appendChild(card);
+}
+
+   // for the images
+function createImageElement(car) {
+
+    const img = document.createElement('img');
+    img.src = car.img;
+    img.alt = 'car img';
+    img.width = 300;
+    img.height = 200;
+    return img;
+}
+
+    // for the card info
+function createCardInfoElement(car) {
+
+    const cardInfo = document.createElement('div');
+    cardInfo.className = 'card-info';
+
+    const heading = document.createElement('h2');
+    heading.textContent = `${car.carMaker} ${car.carModel}`;
+    cardInfo.appendChild(heading);
+
+    for (let vendor in car.price) {
+        const priceParagraph = document.createElement('p');
+        priceParagraph.textContent = `${vendor}: ${car.price[vendor]} NOK/day`;
+        cardInfo.appendChild(priceParagraph);
+    }
+
+    const locationParagraph = document.createElement('p');
+    locationParagraph.textContent = 'Location: Ålesund';
+    cardInfo.appendChild(locationParagraph);
+
+    return cardInfo;
+}
