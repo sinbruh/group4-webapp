ff28d2f6aedf02fe24453ead7ea8668765172552
mathi <mathiaswestergaardjohansen@gmail.com>
2024-03-11 15:36:49 +0100

    Added @Repository annotation to both classes

--- src/main/java/no/ntnu/group4/webapp/RentalRepository.java
+++ src/main/java/no/ntnu/group4/webapp/RentalRepository.java
@@ -3,5 +3,6 @@ package no.ntnu.group4.webapp;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RentalRepository extends CrudRepository<Rental, Integer>{
 }
--- src/main/java/no/ntnu/group4/webapp/UserRepository.java
+++ src/main/java/no/ntnu/group4/webapp/UserRepository.java
@@ -3,6 +3,7 @@ package no.ntnu.group4.webapp;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends CrudRepository<User, Integer>{
 
 }
