c314f9ebbec6d4fdc52dcdcc9bc068a60e6626c5
mathi <mathiaswestergaardjohansen@gmail.com>
2024-03-21 15:27:23 +0100

    Extended RentalController with methods required

--- src/main/java/no/ntnu/group4/webapp/RentalController.java
+++ src/main/java/no/ntnu/group4/webapp/RentalController.java
@@ -1,4 +1,28 @@
 package no.ntnu.group4.webapp;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+
+@RestController
+@RequestMapping ("/rentals")
 public class RentalController {
+    @Autowired
+    private RentalService rentalService;
+
+    @GetMapping()
+    public Iterable<Rental> getAll() {
+        return rentalService.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Rental> getOne(@PathVariable int id) {
+        ResponseEntity<Rental> response;
+        Optional<Rental> rental = rentalService.getOne(id);
+        response = rental.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+        return response;
+    }
 }
