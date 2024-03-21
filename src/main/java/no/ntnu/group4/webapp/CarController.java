30f339ee689a2eaae40dc0d737d59c3a53bc6b67
sinbruh <41494471+sinbruh@users.noreply.github.com>
2024-03-21 10:42:52 +0100

    refactored static files and fixed database

--- src/main/java/no/ntnu/group4/webapp/CarController.java
+++ src/main/java/no/ntnu/group4/webapp/CarController.java
@@ -4,6 +4,8 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,6 +28,18 @@ public class CarController {
       response = ResponseEntity.ok(car.get());
     } else {
       response = ResponseEntity.notFound().build();
+    }
+    return response;
+  }
+
+  @PostMapping("/{id}")
+  public ResponseEntity<Car> addCar(@RequestBody Car car) {
+    ResponseEntity<Car> response;
+    try {
+      int id = carService.addCar(car);
+      response = ResponseEntity.ok(carService.findById(id).get());
+    } catch (IllegalArgumentException e) {
+      response = ResponseEntity.badRequest().build();
     }
     return response;
   }
