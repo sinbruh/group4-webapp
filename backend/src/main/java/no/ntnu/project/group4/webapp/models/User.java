d12018ea4026b83f0e7e9550d178d60d066ef853
Mats Bakketeig <matsbak@stud.ntnu.no>
2024-04-11 14:25:30 +0200

    Refactored User class for security purposes

--- backend/src/main/java/no/ntnu/project/group4/webapp/models/User.java
+++ backend/src/main/java/no/ntnu/project/group4/webapp/models/User.java
@@ -5,11 +5,14 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
 
 import java.sql.Date;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
 import java.util.Set;
 
 @Entity(name = "users")
@@ -28,8 +31,8 @@ public class User {
   private boolean active = true;
   @ManyToMany(fetch = FetchType.EAGER)
   @JoinTable(name = "user_role",
-      joinColumns = @JoinColumn(name = "user_id"),
-      inverseJoinColumns = @JoinColumn(name = "role_id")
+    joinColumns = @JoinColumn(name = "user_id"),
+    inverseJoinColumns = @JoinColumn(name = "role_id")
   )
   private Set<Role> roles = new LinkedHashSet<>();
 
@@ -39,9 +42,8 @@ public class User {
   public User() {
   }
 
-  public User(int id, String firstName, String lastName, String email, int phoneNumber,
-              String password, Date dateOfBirth) {
-    this.id = id;
+  public User(String firstName, String lastName, String email, int phoneNumber, String password,
+              Date dateOfBirth) {
     this.firstName = firstName;
     this.lastName = lastName;
     this.email = email;
@@ -50,11 +52,11 @@ public class User {
     this.dateOfBirth = dateOfBirth;
   }
 
-  public int getId() {
+  public Long getId() {
     return this.id;
   }
 
-  public void setId(int id) {
+  public void setId(Long id) {
     this.id = id;
   }
 
@@ -106,6 +108,66 @@ public class User {
     this.dateOfBirth = dateOfBirth;
   }
 
+  public boolean isActive() {
+    return this.active;
+  }
+
+  public void setActive(boolean active) {
+    this.active = active;
+  }
+
+  public Set<Role> getRoles() {
+    return this.roles;
+  }
+
+  public void setRoles(Set<Role> roles) {
+    this.roles = roles;
+  }
+
+  /**
+   * Adds the specified role to the user.
+   * 
+   * @param role The specified role
+   */
+  public void addRole(Role role) {
+    this.roles.add(role);
+  }
+
+  /**
+   * Checks if the user has the admin role.
+   * 
+   * @return True if the user has the admin role or false otherwise
+   */
+  public boolean isAdmin() {
+    return this.hasRole("ROLE_ADMIN");
+  }
+
+  /**
+   * Checks if the user has the specified role.
+   * 
+   * @param roleName The specified role
+   * @return True if the user has the specified role or false otherwise
+   */
+  private boolean hasRole(String roleName) {
+    boolean found = false;
+    Iterator<Role> it = this.roles.iterator();
+    while (!found && it.hasNext()) {
+      Role role = it.next();
+      if (role.getName().equals(roleName)) {
+        found = true;
+      }
+    }
+    return found;
+  }
+
+  /**
+   * Checks if the user has valid custom fields.
+   * 
+   * <p>Custom fields are in this context fields that are set by the user when the user is
+   * created.</p>
+   * 
+   * @return True if the user has valid custom fields or false otherwise
+   */
   public boolean isValid() {
     return this.firstName != null && !this.firstName.isBlank() && this.lastName != null &&
            !this.lastName.isBlank() && this.email != null && !this.email.isBlank() &&
