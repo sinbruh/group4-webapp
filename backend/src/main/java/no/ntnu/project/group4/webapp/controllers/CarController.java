e56b56988606f69a8d0573a4f5530ef47c5e412f
Mats Bakketeig <matsbak@stud.ntnu.no>
2024-04-11 14:37:40 +0200

    Edited endpoints in CarController class

--- backend/src/main/java/no/ntnu/project/group4/webapp/controllers/CarController.java
+++ backend/src/main/java/no/ntnu/project/group4/webapp/controllers/CarController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import no.ntnu.project.group4.webapp.models.Car;
 import no.ntnu.project.group4.webapp.services.CarService;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api/cars")
 public class CarController {
@@ -25,7 +27,7 @@ public class CarController {
     return carService.getAll();
   }
 
-  @GetMapping("/api/cars/{id}")
+  @GetMapping("/{id}")
   public ResponseEntity<Car> getOne(@PathVariable int id) {
     ResponseEntity<Car> response;
     Optional<Car> car = carService.findById(id);
@@ -37,7 +39,7 @@ public class CarController {
     return response;
   }
 
-  @DeleteMapping("/api/cars/{id}")
+  @DeleteMapping("/{id}")
   public HttpStatus deleteOne(@PathVariable int id) {
     HttpStatus response;
     Optional<Car> car = carService.findById(id);
@@ -47,7 +49,6 @@ public class CarController {
     } else {
       response = HttpStatus.NOT_FOUND;
     }
-
     return response;
   }
 }
