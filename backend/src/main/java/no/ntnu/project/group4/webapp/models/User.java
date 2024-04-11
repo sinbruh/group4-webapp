bcc630b03ce856dbac0ea6163a4105d1bdb041d9
Mats Bakketeig <matsbak@stud.ntnu.no>
2024-04-11 13:57:31 +0200

    Edited User and added Role entity class and updated and refactored POM.xml

--- backend/src/main/java/no/ntnu/project/group4/webapp/models/User.java
+++ backend/src/main/java/no/ntnu/project/group4/webapp/models/User.java
@@ -1,18 +1,22 @@
 package no.ntnu.project.group4.webapp.models;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
 
 import java.sql.Date;
 import java.util.Set;
 
-@Entity
+@Entity(name = "users")
 public class User {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private int id;
+  private Long id;
   private String firstName;
   private String lastName;
   private String email;
@@ -21,7 +25,17 @@ public class User {
   private Date dateOfBirth;
   @OneToMany(mappedBy = "user")
   private Set<Rental> rentals;
+  private boolean active = true;
+  @ManyToMany(fetch = FetchType.EAGER)
+  @JoinTable(name = "user_role",
+      joinColumns = @JoinColumn(name = "user_id"),
+      inverseJoinColumns = @JoinColumn(name = "role_id")
+  )
+  private Set<Role> roles = new LinkedHashSet<>();
 
+  /**
+   * Empty constructor needed for JPA.
+   */
   public User() {
   }
 
