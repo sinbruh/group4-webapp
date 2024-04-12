3784d9a1b88e34a5fd32e6d33f86e00ef1973b19
Mats Bakketeig <matsbak@stud.ntnu.no>
2024-04-12 14:33:19 +0200

    Added DataInitializer class

---
+++ backend/src/main/java/no/ntnu/project/group4/webapp/DataInitializer.java
@@ -0,0 +1,45 @@
+package no.ntnu.project.group4.webapp;
+
+import java.util.List;
+import no.ntnu.project.group4.webapp.models.Role;
+import no.ntnu.project.group4.webapp.repositories.RoleRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+/**
+ * A class which inserts some dummy data into the database, when Spring Boot app has started.
+ */
+@Component
+public class DataInitializer implements ApplicationListener<ApplicationReadyEvent> {
+  @Autowired
+  private RoleRepository roleRepository;
+
+  private final Logger logger = LoggerFactory.getLogger("DataInit");
+
+  /**
+   * This method is called when the application is ready (loaded).
+   *
+   * @param event Event which is not used
+   */
+  @Override
+  public void onApplicationEvent(ApplicationReadyEvent event) {
+    List<Role> existingRoles = roleRepository.findAll();
+    if (existingRoles.isEmpty()) {
+      logger.info("Importing data...");
+
+      Role user = new Role("ROLE_USER");
+      Role admin = new Role("ROLE_ADMIN");
+
+      roleRepository.save(user);
+      roleRepository.save(admin);
+
+      logger.info("Done importing data");
+    } else {
+      logger.info("Roles already in the database, not importing data");
+    }
+  }
+}
