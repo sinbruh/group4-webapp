f11284d457110ee8e161168c2c92702fcf9332e0
Mats Bakketeig <matsbak@stud.ntnu.no>
2024-04-16 15:26:32 +0200

    Extended Configuration entity class more for JPA

--- backend/src/main/java/no/ntnu/project/group4/webapp/models/Configuration.java
+++ backend/src/main/java/no/ntnu/project/group4/webapp/models/Configuration.java
@@ -1,17 +1,22 @@
 package no.ntnu.project.group4.webapp.models;
 
+import java.util.Set;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 
-@Entity
+@Entity(name = "configurations")
 public class Configuration {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
   private String name;
+  @OneToMany(mappedBy = "configuration")
+  private Set<Provider> providers;
   @ManyToOne
   private Model model;
 
@@ -36,6 +41,14 @@ public class Configuration {
 
   public void setName(String name) {
     this.name = name;
+  }
+
+  public Set<Provider> getProviders() {
+    return this.providers;
+  }
+
+  public void setProviders(Set<Provider> providers) {
+    this.providers = providers;
   }
 
   public Model getModel() {
