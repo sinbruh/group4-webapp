813edc3163cf5c8e7a7fc9c3e1a60b4195c6dfd0
Mats Bakketeig <matsbak@stud.ntnu.no>
2024-04-16 11:44:38 +0200

    Began initialization of car data on application start

--- backend/src/main/java/no/ntnu/project/group4/webapp/DataInitializer.java
+++ backend/src/main/java/no/ntnu/project/group4/webapp/DataInitializer.java
@@ -1,8 +1,12 @@
 package no.ntnu.project.group4.webapp;
 
-import java.util.List;
+import no.ntnu.project.group4.webapp.models.Car;
 import no.ntnu.project.group4.webapp.models.Role;
+import no.ntnu.project.group4.webapp.repositories.CarRepository;
 import no.ntnu.project.group4.webapp.repositories.RoleRepository;
+
+import java.util.Iterator;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,29 +21,68 @@ import org.springframework.stereotype.Component;
 public class DataInitializer implements ApplicationListener<ApplicationReadyEvent> {
   @Autowired
   private RoleRepository roleRepository;
+  @Autowired
+  private CarRepository carRepository;
 
   private final Logger logger = LoggerFactory.getLogger("DataInit");
 
   /**
    * This method is called when the application is ready (loaded).
+   * 
+   * <p>Imports the initial data into the database. The initial data includes user roles and
+   * cars.</p>
    *
    * @param event Event which is not used
    */
   @Override
   public void onApplicationEvent(ApplicationReadyEvent event) {
-    List<Role> existingRoles = roleRepository.findAll();
-    if (existingRoles.isEmpty()) {
-      logger.info("Importing data...");
+    logger.info("Importing data...");
+    loadUserRoles();
+    loadCars();
+    logger.info("Done importing data");
+  }
 
+  /**
+   * Loads the user roles into the database if it is not already there.
+   */
+  private void loadUserRoles() {
+    boolean isEmpty = true; // Guard condition
+    Iterable<Role> existingRoles = roleRepository.findAll();
+    Iterator<Role> rolesIt = existingRoles.iterator();
+    if (rolesIt.hasNext()) {
+      isEmpty = false;
+    }
+    logger.info("Loading user role data...");
+    if (isEmpty) {
       Role user = new Role("ROLE_USER");
       Role admin = new Role("ROLE_ADMIN");
 
       roleRepository.save(user);
       roleRepository.save(admin);
 
-      logger.info("Done importing data");
+      logger.info("Done loading user role data");
     } else {
-      logger.info("Roles already in the database, not importing data");
+      logger.info("User roles already in the database, not loading data");
+    }
+  }
+
+  /**
+   * Loads the cars into the database if it is not already there.
+   */
+  private void loadCars() {
+    boolean isEmpty = true; // Guard condition
+    Iterable<Car> existingCars = carRepository.findAll();
+    Iterator<Car> carsIt = existingCars.iterator();
+    if (carsIt.hasNext()) {
+      isEmpty = false;
+    }
+    logger.info("Loading car data...");
+    if (isEmpty) {
+      // TODO Construct car instances for each car and save it in the repository
+
+      logger.info("Done loading car data");
+    } else {
+      logger.info("Cars already in the database, not loading data");
     }
   }
 }
