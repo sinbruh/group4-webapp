c5f729afb22f780b27fb2af3d4666f9a63a7e0a2
Mats Bakketeig <matsbak@stud.ntnu.no>
2024-04-16 15:01:03 +0200

    Extended ExtraFeature entity class for JPA

--- backend/src/main/java/no/ntnu/project/group4/webapp/models/ExtraFeature.java
+++ backend/src/main/java/no/ntnu/project/group4/webapp/models/ExtraFeature.java
@@ -1,8 +1,22 @@
 package no.ntnu.project.group4.webapp.models;
 
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+
+@Entity(name = "extrafeatures")
 public class ExtraFeature {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
   private String name;
+  @ManyToMany(mappedBy = "extrafeatures")
+  private Set<Model> models = new LinkedHashSet<>();
 
   public ExtraFeature() {
   }
@@ -25,5 +39,13 @@ public class ExtraFeature {
 
   public void setName(String name) {
     this.name = name;
+  }
+
+  public Set<Model> getModels() {
+    return this.models;
+  }
+
+  public void setModels(Set<Model> models) {
+    this.models = models;
   }
 }
