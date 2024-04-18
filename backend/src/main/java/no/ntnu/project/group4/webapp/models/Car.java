a6d2ccd85235309d2e636313e227f1526785b644
Mats Bakketeig <matsbak@stud.ntnu.no>
2024-04-18 14:01:14 +0200

    Refactored Car entity class and refactored it for JPA

--- backend/src/main/java/no/ntnu/project/group4/webapp/models/Car.java
+++ backend/src/main/java/no/ntnu/project/group4/webapp/models/Car.java
@@ -1,27 +1,39 @@
 package no.ntnu.project.group4.webapp.models;
 
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToOne;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 
-@Entity(name = "cars")
+@Entity(name = "car")
 public class Car {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
-  private String location;
-  private boolean available;
-  @OneToOne
-  private Model model;
+  private String make;
+  private String model;
+  private int year;
+  @ManyToMany
+  @JoinTable(
+    name = "car_configuration",
+    joinColumns = @JoinColumn(name = "car_id"),
+    inverseJoinColumns = @JoinColumn(name = "configuration_id")
+  )
+  private Set<Configuration> configurations = new LinkedHashSet<>();
 
   public Car() {
   }
 
-  public Car(String location) {
-    this.location = location;
-    this.available = true;
+  public Car(String make, String model, int year) {
+    this.make = make;
+    this.model = model;
+    this.year = year;
   }
 
   public Long getId() {
@@ -32,31 +44,27 @@ public class Car {
     this.id = id;
   }
 
-  public String getLocation() {
-    return this.location;
+  public String getMake() {
+    return this.make;
   }
 
-  public void setLocation(String location) {
-    this.location = location;
+  public void setMake(String make) {
+    this.make = make;
   }
 
-  public boolean isAvailable() {
-    return this.available;
-  }
-
-  public void setAvailable(boolean available) {
-    this.available = available;
-  }
-
-  public Model getModel() {
+  public String getModel() {
     return this.model;
   }
 
-  public void setModel(Model model) {
+  public void setModel(String model) {
     this.model = model;
   }
 
-  public boolean isValid() {
-    return !this.location.isBlank();
+  public int getYear() {
+    return this.year;
+  }
+
+  public void setYear(int year) {
+    this.year = year;
   }
 }
