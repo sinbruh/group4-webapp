d2a8587d1fb15198ac847984c8137f4fc68a043a
Mats Bakketeig <matsbak@stud.ntnu.no>
2024-04-18 21:32:51 +0200

    Changed Car entity class for JPA

--- backend/src/main/java/no/ntnu/project/group4/webapp/models/Car.java
+++ backend/src/main/java/no/ntnu/project/group4/webapp/models/Car.java
@@ -1,16 +1,9 @@
 package no.ntnu.project.group4.webapp.models;
 
-import java.util.LinkedHashSet;
-import java.util.Set;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.ManyToMany;
-
 @Entity(name = "car")
 public class Car {
   @Id
@@ -19,13 +12,6 @@ public class Car {
   private String make;
   private String model;
   private int year;
-  @ManyToMany
-  @JoinTable(
-    name = "car_configuration",
-    joinColumns = @JoinColumn(name = "car_id"),
-    inverseJoinColumns = @JoinColumn(name = "configuration_id")
-  )
-  private Set<Configuration> configurations = new LinkedHashSet<>();
 
   public Car() {
   }
@@ -66,23 +52,6 @@ public class Car {
 
   public void setYear(int year) {
     this.year = year;
-  }
-
-  public Set<Configuration> getConfigurations() {
-    return this.configurations;
-  }
-
-  public void setConfigurations(Set<Configuration> configurations) {
-    this.configurations = configurations;
-  }
-
-  /**
-   * Adds the specified configuraiton to the car.
-   * 
-   * @param configuration The specified configuration
-   */
-  public void addConfiguration(Configuration configuration) {
-    this.configurations.add(configuration);
   }
 
   public boolean isValid() {
