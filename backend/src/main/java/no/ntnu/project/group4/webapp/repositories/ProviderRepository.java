3b5e11cbf8562f3f80ef4d244a6a555877f003f4
Mats Bakketeig <matsbak@stud.ntnu.no>
2024-04-18 16:08:59 +0200

    Added multiple repository classes

--- backend/src/main/java/no/ntnu/project/group4/webapp/repositories/ProviderRepository.java
+++ backend/src/main/java/no/ntnu/project/group4/webapp/repositories/ProviderRepository.java
@@ -3,8 +3,8 @@ package no.ntnu.project.group4.webapp.repositories;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import no.ntnu.project.group4.webapp.models.Car;
+import no.ntnu.project.group4.webapp.models.Provider;
 
 @Repository
-public interface CarRepository extends CrudRepository<Car, Long> {
+public interface ProviderRepository extends CrudRepository<Provider, Long> {
 }
