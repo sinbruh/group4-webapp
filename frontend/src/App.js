22c8341c761c33203c9f79121ab7a33d0ab79f53
mathi <mathiaswestergaardjohansen@gmail.com>
2024-04-26 13:54:44 +0200

    added button to component that calls the togglePop function when clicked

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,16 +4,29 @@ import About from './About';
 import Contact from './Contact';
 import Index from './Index';
 import Search from './Search';
+import ProfileIcon from './ProfileIcon';
 
 function App() {
+
+    const [seen, setSeen] = React.useState(false);
+
+    function togglePop() {
+        setSeen(!seen);
+    };
+
     return (
+        <div>
+            <button onClick={togglePop}>Login</button>
+            {seen ? <Login toggle={{togglePop}} /> : null}
+        </div>
         <Router>
-        <Switch>
-            <Route path="/about" component={About} />
-            <Route path="/contact" component={Contact} />
-            <Route path="/search" component={Search} />
-            <Route path="/" component={Index} />
-        </Switch>
+            <ProfileIcon />
+            <Switch>
+                <Route path="/about" component={About} />
+                <Route path="/contact" component={Contact} />
+                <Route path="/search" component={Search} />
+                <Route path="/" component={Index} />
+            </Switch>
         </Router>
     );
 }
