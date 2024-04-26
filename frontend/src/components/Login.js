e85bfbb7c4bcf41602c848ab63f39a4d28fc20ea
mathi <mathiaswestergaardjohansen@gmail.com>
2024-04-26 13:57:26 +0200

    added handleLogin function to take care of the login logic

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -4,4 +4,12 @@ import React from 'react';
 function Login(props) {
     const [username, setUsername] = React.useState('');
     const [password, setPassword] = React.useState('');
+
+    function handleLogin(e) {
+        e.preventDefault();
+        // code to handle login goes here
+        props.toggle
+    }
+
+
 }
