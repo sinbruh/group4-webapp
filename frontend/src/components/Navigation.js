4e987d21380153ec5c8fba84d2096339d04b9c39
mathi <mathiaswestergaardjohansen@gmail.com>
2024-05-02 13:49:00 +0200

    Replaced User icon with a temporary button aswell as added margin to move the different tabs few spaces to the left

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -21,38 +21,26 @@ export function Navigation() {
             <nav>
                 <p>
                     <b>
-                        <Link href="/">Home</Link>
+                        <Link href="/" style={{marginRight: '100px'}}>Home</Link>
                     </b>
                 </p>
                 <p>
                     <b>
-                        <Link href="about">About</Link>
+                        <Link href="about" style={{marginRight: '100px'}}>About</Link>
                     </b>
                 </p>
                 <p>
                     <b>
-                        <Link href="contact">Contact</Link>
+                        <Link href="contact" style={{marginRight: '100px'}}>Contact</Link>
+                    </b>
+                </p>
+                <p>
+                    <b>
+                        <button style={{marginRight: '100px'}}>Login</button>
                     </b>
                 </p>
             </nav>
-            {/* User icon */}
-            <svg
-                id="user-icon"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="#000000"
-                className="w-6 h-6"
-                width={60}
-                height={60}
-            >
-                <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
-                />
-            </svg>
+
         </div>
     )
 }
