f555bba2cb33b9b88ce80bdb7dc76cdb719baf85
Mats Bakketeig <matsbak@stud.ntnu.no>
2024-05-02 12:46:29 +0200

    Refactored endpoints in Car and Configuration controller classes

--- backend/src/main/java/no/ntnu/project/group4/webapp/controllers/ConfigurationController.java
+++ backend/src/main/java/no/ntnu/project/group4/webapp/controllers/ConfigurationController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,7 +30,7 @@ public class ConfigurationController {
   }
 
   @GetMapping("/get/{id}")
-  public ResponseEntity<Configuration> getOne(@PathVariable Long id) {
+  public ResponseEntity<Configuration> get(@PathVariable Long id) {
     ResponseEntity<Configuration> response;
     Optional<Configuration> configuration = this.configurationService.getOne(id);
     if (configuration.isPresent()) {
@@ -39,15 +41,39 @@ public class ConfigurationController {
     return response;
   }
 
+  /**
+   * Adds the specified configuration to the database.
+   * 
+   * @param configuration The specified configuration
+   * @return 201 CREATED on success or 400 BAD REQUEST on error
+   */
+  @PostMapping("/add/")
+  public ResponseEntity<String> add(@RequestBody Configuration configuration) {
+    ResponseEntity<String> response;
+    try {
+      this.configurationService.add(configuration);
+      response = new ResponseEntity<>(HttpStatus.CREATED);
+    } catch (Exception e) {
+      response = new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+    return response;
+  }
+
+  /**
+   * Deletes the configuration with the specified ID from the database.
+   * 
+   * @param id The specified ID
+   * @return 200 OK on success or 404 NOT FOUND on error
+   */
   @DeleteMapping("/del/{id}")
-  public HttpStatus deleteOne(@PathVariable Long id) {
-    HttpStatus response;
+  public ResponseEntity<String> delete(@PathVariable Long id) {
+    ResponseEntity<String> response;
     Optional<Configuration> configuration = this.configurationService.getOne(id);
     if (configuration.isPresent()) {
       this.configurationService.delete(id);
-      response = HttpStatus.ACCEPTED;
+      response = new ResponseEntity<>(HttpStatus.OK);
     } else {
-      response = HttpStatus.NOT_FOUND;
+      response = new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
     return response;
   }
