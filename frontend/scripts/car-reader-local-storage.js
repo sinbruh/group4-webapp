eb07ab6a169accc7d0582aed726755bdc3f95039
Leander-Haddal <122444622+Leander-Haddal@users.noreply.github.com>
2024-05-02 15:40:33 +0200

    Changes to parser, reader, json to fit new database structure

--- frontend/scripts/car-reader-local-storage.js
+++ frontend/scripts/car-reader-local-storage.js
@@ -34,7 +34,8 @@ function createCards(data) {
     // Creates a card for each car with the createCard method
     data.cars.forEach(car => {
         // Gets lowest price for a car and checks it to price range
-        const lowestPrice = Math.min(...Object.values(car.price));
+        const prices = car.configurations[0].providers.map(provider => provider.price);
+        const lowestPrice = Math.min(...prices);
         
         const fromPrice = parseInt(document.getElementById('fromPrice').value);
         const toPrice = parseInt(document.getElementById('toPrice').value);
@@ -45,10 +46,45 @@ function createCards(data) {
     });
 }
 
-window.onload = function() {
-        // refreshes the cards when the page is loaded
+
+
+
+
+// for the images
+const createImageElement = (car) => {
+    const img = document.createElement('img');
+    img.src = car.configurations[0].img;
+    img.alt = 'car img';
+    img.width = 300;
+    img.height = 200;
+    return img;
+}
+
+// for the card info
+const createCardInfoElement = (car) => {
+    const cardInfo = document.createElement('div');
+    cardInfo.className = 'card-info';
+
+    const heading = document.createElement('h2');
+    heading.textContent = `${car.make} ${car.model}`;
+    cardInfo.appendChild(heading);
+
+    for (let provider of car.configurations[0].providers) {
+        const priceParagraph = document.createElement('p');
+        priceParagraph.textContent = `${provider.name}: ${provider.price} NOK/day`;
+        cardInfo.appendChild(priceParagraph);
+    }
+
+    const locationParagraph = document.createElement('p');
+    locationParagraph.textContent = `Location: ${car.configurations[0].location}`;
+    cardInfo.appendChild(locationParagraph);
+
+    return cardInfo;
+}
+
+window.initCarReader = function() {
+        // called when parseSpringBootData.js is done
     fetchAndCreateCards();
-    
        // refreshes cards when price range is changed
     document.getElementById('fromPrice').addEventListener('change', fetchAndCreateCards);
     document.getElementById('toPrice').addEventListener('change', fetchAndCreateCards);
@@ -69,55 +105,22 @@ window.onload = function() {
     
     
         //method for expanded card
-        card.addEventListener('click', function() {
-            const expandedCard = document.getElementById('expandedCard');
-            expandedCard.innerHTML = card.innerHTML;
-    
-            //obtains info from original card, and adds more info from Json
-            const additionalInfo = document.createElement('div');
-            additionalInfo.innerHTML = `<p>Year: ${car.year}</p>
-                            <p>Fuel Type: ${car.fuelType}</p>
-                            <p>Transmission Type: ${car.transmissionType}</p>
-                            <p>Number of Seats: ${car.numberOfSeats}</p>
-                            <p>Extra Features: ${car.extraFeatures.join(', ')}</p>`;
-            expandedCard.appendChild(additionalInfo);
-        });
-    
-        document.getElementById('cards').appendChild(card);
+                card.addEventListener('click', function() {
+                    const expandedCard = document.getElementById('expandedCard');
+                    expandedCard.innerHTML = card.innerHTML;
+            
+                 //obtains info from original card, and adds more info from Json
+        const additionalInfo = document.createElement('div');
+        additionalInfo.innerHTML = `<p>Year: ${car.year}</p>
+                        <p>Fuel Type: ${car.configurations[0].fuelType}</p>
+                        <p>Transmission Type: ${car.configurations[0].tranmissionType}</p>
+                        <p>Number of Seats: ${car.configurations[0].numberOfSeats}</p>
+                        <p>Extra Features: ${car.configurations[0].extraFeatures.map(feature => feature.name).join(', ')}</p>`;
+        expandedCard.appendChild(additionalInfo);
+
+    });
+
+    document.getElementById('cards').appendChild(card);
     }
 
-    
-       // for the images
-    function createImageElement(car) {
-    
-        const img = document.createElement('img');
-        img.src = car.img;
-        img.alt = 'car img';
-        img.width = 300;
-        img.height = 200;
-        return img;
-    }
-    
-        // for the card info
-    function createCardInfoElement(car) {
-    
-        const cardInfo = document.createElement('div');
-        cardInfo.className = 'card-info';
-    
-        const heading = document.createElement('h2');
-        heading.textContent = `${car.carMaker} ${car.carModel}`;
-        cardInfo.appendChild(heading);
-    
-        for (let vendor in car.price) {
-            const priceParagraph = document.createElement('p');
-            priceParagraph.textContent = `${vendor}: ${car.price[vendor]} NOK/day`;
-            cardInfo.appendChild(priceParagraph);
-        }
-    
-        const locationParagraph = document.createElement('p');
-        locationParagraph.textContent = 'Location: Ålesund';
-        cardInfo.appendChild(locationParagraph);
-    
-        return cardInfo;
-    }
     
