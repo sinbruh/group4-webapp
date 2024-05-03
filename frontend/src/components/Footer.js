16190b24c5d63fdc34369807ef4db1f9edf2b4ab
sinbruh <sindre999@hotmail.com>
2024-05-03 15:27:53 +0200

    did more work on frontpage and search page

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import Image from "next/image";
 import ntnu from "@/img/ntnu-logo.png";
+import Link from "next/link";
 
 export function Footer() {
-    return (<footer className="bg-[#00509E] gap-y-10px flex-row justify-center items-center" >
+    return (<footer className="bg-[#00509E] gap-y-10px p-5 flex-row justify-center items-center" >
         <div className="flex flex-row justify-center items-center">
             <Image src={ntnu} alt={"NTNU"} width={379} height={70}/>
         </div>
-            <p>
-                <em>
+            <p className="text-center">
+                <em className="text-white">
                     This website is a result of a university group project, performed in
-                    the course <a href="https://www.ntnu.edu/studies/courses/IDATA2301#tab=omEmnet">
+                    the course <Link className="text-blue-300 underline" href="https://www.ntnu.edu/studies/courses/IDATA2301#tab=omEmnet">
                         IDATA2301 Web
-                        <br/> Technologies </a>, at <a href="https://www.ntnu.edu/">NTNU</a>. All the information
+                        <br/> Technologies </Link>, at <Link href="https://www.ntnu.edu/">NTNU</Link>. All the information
                     provided here is a result of imagination. Any
                     <br/>
                     resemblance with real companies or products is a coincidence.
