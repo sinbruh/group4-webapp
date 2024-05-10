2dc88424335103fd26a5cc426e42a912d914e99b
sinbruh <sindre999@hotmail.com>
2024-05-10 15:44:29 +0200

    added icons and styling to cards

--- frontend/src/components/CarCard.js
+++ frontend/src/components/CarCard.js
@@ -1,6 +1,11 @@
 import * as React from "react"
 import Image from "next/image"
 import carImage from "@/img/cars/BMW-M3.jpg"
+import carIcon from "@/img/icons/car.svg";
+import checkIcon from "@/img/icons/checkmark.svg";
+import closeIcon from "@/img/icons/close.svg";
+import cogIcon from "@/img/icons/cog.svg";
+import peopleIcon from "@/img/icons/people.svg";
 
 import {
     Card,
@@ -21,15 +26,17 @@ const CarCard = ({ carName, price, location, size, fuelType, transmission, descr
                     <CardDescription>{description}</CardDescription>
                 </CardHeader>
                 <CardContent>
-                    {availability ? "available" : "unavailable"}
+                    {availability ?
+                        <div className="text-[#326D0D] font-bold flex gap-2 items-center"><Image src={checkIcon} width={32} height={32} /> Available </div> :
+                        <div className="text-red-500 font-bold flex gap-2 items-center"><Image src={closeIcon} width={32} height={32} /> Unavailable </div>}
                     <p>Price: {price} NOK/day</p>
                     <p>Location: {location}</p>
                 </CardContent>
                 <CardFooter className="flex flex-row">
-                    <div>
-                        <div>Size: {size}</div>
-                        <div>Fuel type: {fuelType}</div>
-                        <div>Transmission: {transmission}</div>
+                    <div className="flex gap-2">
+                        <div className="flex items-center"><Image src={peopleIcon} alt="people" width={32} height={32}/> {size}</div>
+                        <div className="flex items-center"><Image src={carIcon} alt="fuel type" widht={32} height={32} /> {fuelType}</div>
+                        <div className="flex items-center"><Image src={cogIcon} alt="transmission" width={32} height={32} /> {transmission}</div>
                     </div>
                 </CardFooter>
             </div>
