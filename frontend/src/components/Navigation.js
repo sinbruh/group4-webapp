f1757d055fe488adc429171d31b290752b38ac86
mathi <mathiawj@stud.ntnu.no>
2024-05-11 17:44:51 +0200

    did necessary changes for the navigation class, including replacing the profile icon with a temporary button that will serve as the login/register function. Changes will be made once everything is properly up and running.

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -1,8 +1,17 @@
-import React from "react";
+'use client';
+import React, {useState} from "react";
 import Link from "next/link";
 import logo from "@/img/temp-logo.png";
 import Image from "next/image";
+import { LoginModalClient } from "@/app/modal/LoginModal.client";
+
+
+
 export function Navigation() {
+
+    let isLoginOpen, setIsLoginOpen;
+    [isLoginOpen, setIsLoginOpen] = useState(false);
+
     return (
         <div id="nav-container" className=" p-2 bg-white min-h-16 flex items-center justify-between">
             <header className="flex items-center">
@@ -36,23 +45,13 @@ export function Navigation() {
                 </p>
             </nav>
             {/* User icon */}
-            <svg
-                id="user-icon"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="#000000"
-                className="w-6 h-6"
-                width={60}
-                height={60}
+            <button
+                className="btn btn-primary text-white bg-green-800 hover:bg-green-500 font-bold py-2 px-4 rounded-full"
+                onClick={() => setIsLoginOpen(true)}
             >
-                <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
-                />
-            </svg>
+                Login
+            </button>
+            {isLoginOpen && <LoginModalClient />}
         </div>
     )
 }
