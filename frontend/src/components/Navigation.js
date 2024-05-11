829bb3b25c6c0f099aebd49dc136d05af8ec3ac2
sinbruh <sindre999@hotmail.com>
2024-05-11 02:21:16 +0200

    updated user icon

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -2,6 +2,7 @@ import React from "react";
 import Link from "next/link";
 import logo from "@/img/temp-logo.png";
 import Image from "next/image";
+import userIcon from "@/img/icons/person.svg";
 export function Navigation() {
     return (
         <div id="nav-container" className=" p-2 bg-white min-h-16 flex items-center justify-between">
@@ -15,7 +16,7 @@ export function Navigation() {
                     />
                 </Link>
                 <Link href="/">
-                    <h1 className="px-2">Rental Roulette</h1>
+                    <h1 className="px-2 font-bold">Rental Roulette</h1>
                 </Link>
             </header>
             <nav className="flex items-center justify-center grow gap-x-10 m-10px">
@@ -36,23 +37,7 @@ export function Navigation() {
                 </p>
             </nav>
             {/* User icon */}
-            <svg
-                id="user-icon"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="#000000"
-                className="w-6 h-6"
-                width={60}
-                height={60}
-            >
-                <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
-                />
-            </svg>
+        <Image src={userIcon} alt="User icon" width={32} height={32} />
         </div>
     )
 }
