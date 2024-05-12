6d87688375ca96f3c56000e5d01f4f9db3661318
mathi <mathiawj@stud.ntnu.no>
2024-05-12 21:35:58 +0200

    fixed the colouring of the login button to fit more with the website

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -47,7 +47,7 @@ export function Navigation() {
             </nav>
             {/* User icon */}
             <button
-                className="btn btn-primary text-white bg-green-800 hover:bg-green-500 font-bold py-2 px-4 rounded-full"
+                className="btn btn-primary text-white bg-blue-950 hover:bg-blue-800 font-bold py-2 px-4 rounded-full"
                 onClick={() => setIsLoginOpen(true)}
             >
                 Login
