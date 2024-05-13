0956445b2a9558697fed279b03381dfce0eedf04
mathi <mathiaswestergaardjohansen@gmail.com>
2024-05-13 14:58:16 +0200

    methods for sending an asynchronous request to the remote API and checking if it has an okay status

---
+++ frontend/src/tools/request.js
@@ -0,0 +1,35 @@
+import {getCookie} from "@/tools/cookies";
+import {HttpResponseError} from "@/tools/HttpResponseError";
+
+const API_URL = process.env.VUE_APP_API_URL;
+
+export function asyncApiRequest(method, url, requestBody = null, returnPlainText = false) {
+    const fullUrl = API_BASE_URL + url;
+    let body = null;
+    let headers = {};
+    if (method.toLowerCase() !== "get" && requestBody) {
+        headers["Content-Type"] = "application/json";
+        body = JSON.stringify(requestBody);
+    }
+    const jwtToken = getCookie("jwt");
+    if (jwtToken) {
+        headers["Authorization"] = "Bearer" + jwtToken;
+    }
+
+    return fetch(fullUrl, {
+        method: method,
+        mode: "cors",
+        headers: headers,
+        body: body,
+    })
+        .then(handleErrors)
+        .then((response) => returnPlainText ? response : response.json());
+}
+
+async function handleErrors(response) {
+    if (!response.ok) {
+        const responseText = await response.text();
+        throw new HttpResponseError(response.status, responseText);
+    }
+    return response;
+}
