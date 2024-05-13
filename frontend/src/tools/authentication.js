7e5044fcd7a02ac6a0555b1dd86a54a7469d362b
mathi <mathiaswestergaardjohansen@gmail.com>
2024-05-13 16:23:03 +0200

    authentication class was mistakenly implemented, this is the correct version.

--- frontend/src/tools/authentication.js
+++ frontend/src/tools/authentication.js
@@ -1,37 +1,82 @@
-import {getCookies} from './cookies';
-import {HttpResponseError} from './HttpResponseError';
+import {deleteCookie, getCookie, setCookie} from "./cookies";
+import {asyncApiRequest} from "./request";
 
-const API_URL = process.env.REACT_APP_API_URL;
-
-export function asyncApiRequest(method, url, requestBody = null, returnPlainText = false) {
-    const fullUrl = API_URL + url;
-    let body = null;
-    let headers = {};
-    if (method.toLowerCase() !== "get" && requestBody) {
-        headers["Contect-Type"] = "application/json";
-        body = JSON.stringify(requestBody);
+export function getAuthenticatedUser() {
+    let user = null;
+    const username = getCookie("current_username");
+    const commaSeparatedRoles = getCookie("current_user_roles");
+    if (username && commaSeparatedRoles) {
+        const roles = commaSeparatedRoles.split(",");
+        user = {
+            username: username,
+            roles: roles,
+        };
     }
-    const jwtToken = getCookie("jwt");
-    if (jwtToken) {
-        headers["Authorization"] = 'Bearer' + jwtToken;
-    }
-
-    return fetch(fullUrl, {
-        method: method,
-        mode: "cors",
-        headers: headers,
-        body: body,
-    })
-        .then(handleErrors)
-        .then((response) => returnPlainText ? response : response.json());
+    return user;
 }
 
-async function handleErrors(response) {
-    if (!response.ok) {
-        const responseText = await response.text();
-        throw new HttpResponseError(response.status, responseText);
-    }
-    return response;
+export function isAdmin(user) {
+    return user && user.roles && user.roles.includes("admin");
 }
+
+export async function sendAuthenticationRequest(
+    username,
+    password,
+    successCallBack,
+    errorCallBack
+){
+    const postData = {
+        username: username,
+        password: password,
+    };
+    try {
+        const jwtResponse = await asyncApiRequest("POST", "/authenticate", postData);
+        if (jwtResponse && jwtResponse.jwt) {
+            setCookie("jwt", jwtResponse.jwt);
+            if (userData) {
+                setCookie("current_username", userData.username);
+                setCookie("current_user_roles", userData.roles.join(","));
+                successCallBack(userData);
+            }
+        }
+    } catch (httpError) {
+        errorCallBack(httpError);
+    }
+}
+
+function parseJwt(token) {
+    const base64Url = token.split(".")[1];
+    const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+    const jsonPayload = decodeURIComponent(
+        atob(base64)
+            .split("")
+            .map(function (c) {
+                return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+            })
+            .join("")
+    );
+    return JSON.parse(jsonPayload);
+}
+
+function parseJwtUser(jwtString) {
+    let user = null;
+    const jwtObject = parseJwt(jwtString);
+    if (jwtObject) {
+        user = {
+            username: jwtObject.sub,
+            roles: jwtObject.roles.map((r) => r.authority),
+        };
+    }
+    return user;
+}
+
+export function deleteAuthorizationCookies() {
+    deleteCookie("jwt");
+    deleteCookie("current_username");
+    deleteCookie("current_user_roles");
+}
+
+
+
 
 
