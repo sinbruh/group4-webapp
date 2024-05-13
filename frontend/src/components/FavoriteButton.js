ccc3077a6964cf1094dc55a869f7af1b1f73ac83
sinbruh <sindre999@hotmail.com>
2024-05-14 00:10:42 +0200

    added tooltip to favorite button

--- frontend/src/components/FavoriteButton.js
+++ frontend/src/components/FavoriteButton.js
@@ -1,16 +1,31 @@
 "use client"
-import React, {useState} from "react";
+import React, { useState } from "react";
 import favoriteIcon from "@/img/icons/star.svg";
 import favoriteIconOutline from "@/img/icons/star-outline.svg";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import {
+    Tooltip,
+    TooltipContent,
+    TooltipProvider,
+    TooltipTrigger,
+} from "@/components/ui/tooltip";
 
 export default function FavoriteButton() {
     const [isFavorite, setIsFavorite] = useState(false);
 
     return (
-        <Button variant="ghost" size="icon" onClick={() => setIsFavorite(!isFavorite)}>
-            <Image src={isFavorite ? favoriteIcon : favoriteIconOutline} width={32} height={32} alt="favorite" />
-        </Button>
+        <TooltipProvider>
+            <Tooltip>
+                <TooltipTrigger asChild>
+                    <Button variant="ghost" size="icon" onClick={() => setIsFavorite(!isFavorite)}>
+                        <Image src={isFavorite ? favoriteIcon : favoriteIconOutline} width={32} height={32} alt="favorite" />
+                    </Button>
+                </TooltipTrigger>
+                <TooltipContent>
+                    <p>Add to favorite</p>
+                </TooltipContent>
+            </Tooltip>
+        </TooltipProvider>
     );
 }
