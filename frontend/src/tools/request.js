3e03ff37dcc4f0efffa05ede945c22f91da8005a
mathi <mathiaswestergaardjohansen@gmail.com>
2024-05-13 16:23:29 +0200

    changed const API_URL to API_BASE_URL

--- frontend/src/tools/request.js
+++ frontend/src/tools/request.js
@@ -1,7 +1,7 @@
 import {getCookie} from "@/tools/cookies";
 import {HttpResponseError} from "@/tools/HttpResponseError";
 
-const API_URL = process.env.VUE_APP_API_URL;
+const API_BASE_URL = process.env.VUE_APP_API_URL;
 
 export function asyncApiRequest(method, url, requestBody = null, returnPlainText = false) {
     const fullUrl = API_BASE_URL + url;
