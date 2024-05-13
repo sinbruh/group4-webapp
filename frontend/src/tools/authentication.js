88dec164f45f5af612f671919daa09e2998d9d0b
mathi <mathiaswestergaardjohansen@gmail.com>
2024-05-13 14:35:04 +0200

    implemented method for authentication.js: API request and handleErrors

--- frontend/src/tools/authentication.js
+++ frontend/src/tools/authentication.js
@@ -1,1 +1,37 @@
-import 
+import {getCookies} from './cookies';
+import {HttpResponseError} from './HttpResponseError';
+
+const API_URL = process.env.REACT_APP_API_URL;
+
+export function asyncApiRequest(method, url, requestBody = null, returnPlainText = false) {
+    const fullUrl = API_URL + url;
+    let body = null;
+    let headers = {};
+    if (method.toLowerCase() !== "get" && requestBody) {
+        headers["Contect-Type"] = "application/json";
+        body = JSON.stringify(requestBody);
+    }
+    const jwtToken = getCookie("jwt");
+    if (jwtToken) {
+        headers["Authorization"] = 'Bearer' + jwtToken;
+    }
+
+    return fetch(fullUrl, {
+        method: method,
+        mode: "cors",
+        headers: headers,
+        body: body,
+    })
+        .then(handleErrors)
+        .then((response) => returnPlainText ? response : response.json());
+}
+
+async function handleErrors(response) {
+    if (!response.ok) {
+        const responseText = await response.text();
+        throw new HttpResponseError(response.status, responseText);
+    }
+    return response;
+}
+
+
