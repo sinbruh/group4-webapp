b645a306e343ce20621da0cbbd03fef8fc696169
sinbruh <sindre999@hotmail.com>
2024-05-14 17:01:21 +0200

    improved sign up modal

--- frontend/src/components/modal/LoginModal.client.jsx
+++ frontend/src/components/modal/LoginModal.client.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import SignupModal from './SignUpModal.jsx';
 
+
 export function LoginModalClient({ isOpen, onClose }) {
     const [formData, setFormData] = useState({ 'username': '', 'password': '' });
     const [errors, setErrors] = useState(null);
