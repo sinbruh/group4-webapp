c0c93b2cf0df980d715c1bf3067f1fd519fa2286
sinbruh <41494471+sinbruh@users.noreply.github.com>
2024-05-19 19:40:11 +0200

    added check for user status in favorite button

--- frontend/src/components/FavoriteButton.js
+++ frontend/src/components/FavoriteButton.js
@@ -11,31 +11,42 @@ import {
     TooltipTrigger,
 } from "@/components/ui/tooltip";
 
+import { useStore } from "@/tools/authentication";
+import { UserNotLoggedInAlert } from "@/components/alerts/UserNotLoggedInAlert";
+
 export default function FavoriteButton({ configID }) {
     const [isFavorite, setIsFavorite] = useState(false);
+    const user = useStore((state) => state.user);
+    const [isAlertOpen, setIsAlertOpen] = useState(false);
 
     const handleOnClick = (e) => {
         console.log("Favorite button clicked");
         e.stopPropagation();
-        //toglle the favorite state on or off
-        setIsFavorite(!isFavorite);
 
-        //send request to backend to add to favorite with configID
-
+        if (user) {
+            setIsFavorite(!isFavorite);
+            //send request to backend to add to favorite with configID
+        } else {
+            console.log("User not logged in");
+            setIsAlertOpen(true);
+        }
     };
 
     return (
-        <TooltipProvider>
-            <Tooltip>
-                <TooltipTrigger asChild>
-                    <Button variant="ghost" size="icon" onClick={(e) => handleOnClick(e)}>
-                        <Image src={isFavorite ? favoriteIcon : favoriteIconOutline} width={32} height={32} alt="favorite" />
-                    </Button>
-                </TooltipTrigger>
-                <TooltipContent>
-                    <p>Add to favorite</p>
-                </TooltipContent>
-            </Tooltip>
-        </TooltipProvider>
+        <div>
+            <UserNotLoggedInAlert isOpen={isAlertOpen} setIsOpen={setIsAlertOpen} />
+            <TooltipProvider>
+                <Tooltip>
+                    <TooltipTrigger asChild>
+                        <Button variant="ghost" size="icon" onClick={(e) => handleOnClick(e)}>
+                            <Image src={isFavorite ? favoriteIcon : favoriteIconOutline} width={32} height={32} alt="favorite" />
+                        </Button>
+                    </TooltipTrigger>
+                    <TooltipContent>
+                        <p>Add to favorite</p>
+                    </TooltipContent>
+                </Tooltip>
+            </TooltipProvider>
+        </div>
     );
 }
