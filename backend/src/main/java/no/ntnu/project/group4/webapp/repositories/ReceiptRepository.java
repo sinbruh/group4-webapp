6140bdc4cc784b3b41142a8b0b2410a898a96e8c
Mats Bakketeig <matsbak@stud.ntnu.no>
2024-05-20 23:42:40 +0200

    Added JavaDoc in Receipt repository class

--- backend/src/main/java/no/ntnu/project/group4/webapp/repositories/ReceiptRepository.java
+++ backend/src/main/java/no/ntnu/project/group4/webapp/repositories/ReceiptRepository.java
@@ -5,6 +5,12 @@ import org.springframework.stereotype.Repository;
 
 import no.ntnu.project.group4.webapp.models.Receipt;
 
+/**
+ * The ReceiptRepository class represents the repository class for the receipt entity.  
+ * 
+ * @author Group 4
+ * @version v1.0 (2024.05.20)
+ */
 @Repository
 public interface ReceiptRepository extends CrudRepository<Receipt, Long> {
 }
