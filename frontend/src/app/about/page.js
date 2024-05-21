acbae1eca026b7e980ad4722c29f7109d4c2ecbc
mathi <mathiaswestergaardjohansen@gmail.com>
2024-05-21 16:00:54 +0200

    added basic styling to about page, subject to change in near future

--- frontend/src/app/about/page.js
+++ frontend/src/app/about/page.js
@@ -5,46 +5,50 @@ import Link from "next/link";
 
 export default function Page() {
     return (
-        <div className="">
+        <div className="bg-[url('/temp-background-image-low.webp')] bg-cover bg-center">
             <Navigation />
-            <section className="text-black">
-                <p>
-                    <Link href="/">Home</Link> &gt; <Link href="/about">About</Link>
+            <section className="text-gray-600 body-font">
+                <p className="text-gray-500 mb-3">
+                    <Link href="/" className="text-indigo-500 underline">Home</Link> &gt; <Link href="/about" className="text-indigo-500 underline">About</Link>
                 </p>
             </section>
-            <section className="m-2 p-2 bg-white h-dvh rounded ">
-                <h1 className="text-xl">About Rental Roulette</h1>
-                <p>
-                    Welcome to Rental Roulette, where your journey begins with a spin of the wheel and ends with
-                    the perfect ride tailored just for you. At Rental Roulette, we've revolutionized the car rental
-                    experience by aggregating prices from a myriad of trusted third-party car rental companies. No
-                    more hopping from site to site in search of the best deal &mdash; our platform puts the power
-                    back in your hands. Whether you're embarking on a spontaneous road trip, planning a family
-                    vacation, or simply need a reliable set of wheels for your daily commute, Rental Roulette
-                    ensures that you're always in the driver's seat when it comes to choice and affordability.
-                </p>
-                <br />
-                <p>
-                    Our commitment extends beyond just providing competitive prices; we prioritize transparency,
-                    convenience, and a touch of excitement. With our user-friendly interface, you can effortlessly
-                    compare options, finding the ideal vehicle to suit your preferences and budget. Say goodbye to
-                    hidden fees and hello to a seamless rental experience. At Rental Roulette, we believe that
-                    every journey should begin with a sense of adventure, and our platform is designed to add an
-                    element of thrill to the otherwise mundane task of renting a car. Join us in the pursuit of
-                    hassle-free, cost-effective, and enjoyable travels &mdash; where every rental is a winning
-                    spin!
-                </p>
-                <br />
-                <p>
-                    Nestled in the picturesque landscapes of Ålesund, Rental Roulette takes pride in catering
-                    specifically to the vibrant community of this stunning coastal town. Our focus is on providing
-                    tailored car rental solutions by aggregating prices exclusively from rental companies in the
-                    Ålesund area. Whether you're exploring the fjords, embarking on a scenic coastal drive, or
-                    simply navigating the charming streets of Ålesund itself, Rental Roulette ensures that your
-                    local car rental experience is not only convenient but also intimately connected to the unique
-                    beauty of this Norwegian gem. Embrace the essence of Ålesund with us, where every rental is a
-                    seamless blend of convenience and local charm.
-                </p>
+            <section className="text-gray-600 body-font">
+                <div className="container px-5 py-24 mx-auto">
+                    <div className="xl:w-1/2 lg:w-3/4 w-full mx-auto text-center bg-white bg-opacity-70 p-8 rounded-lg shadow-md">
+                        <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-950">About Rental Roulette</h1>
+                        <p className="leading-relaxed text-lg">
+                            Welcome to Rental Roulette, where your journey begins with a spin of the wheel and ends with
+                            the perfect ride tailored just for you. At Rental Roulette, we've revolutionized the car rental
+                            experience by aggregating prices from a myriad of trusted third-party car rental companies. No
+                            more hopping from site to site in search of the best deal &mdash; our platform puts the power
+                            back in your hands. Whether you're embarking on a spontaneous road trip, planning a family
+                            vacation, or simply need a reliable set of wheels for your daily commute, Rental Roulette
+                            ensures that you're always in the driver's seat when it comes to choice and affordability.
+                        </p>
+                        <br />
+                        <p className="leading-relaxed text-lg">
+                            Our commitment extends beyond just providing competitive prices; we prioritize transparency,
+                            convenience, and a touch of excitement. With our user-friendly interface, you can effortlessly
+                            compare options, finding the ideal vehicle to suit your preferences and budget. Say goodbye to
+                            hidden fees and hello to a seamless rental experience. At Rental Roulette, we believe that
+                            every journey should begin with a sense of adventure, and our platform is designed to add an
+                            element of thrill to the otherwise mundane task of renting a car. Join us in the pursuit of
+                            hassle-free, cost-effective, and enjoyable travels &mdash; where every rental is a winning
+                            spin!
+                        </p>
+                        <br />
+                        <p className="leading-relaxed text-lg">
+                            Nestled in the picturesque landscapes of Ålesund, Rental Roulette takes pride in catering
+                            specifically to the vibrant community of this stunning coastal town. Our focus is on providing
+                            tailored car rental solutions by aggregating prices exclusively from rental companies in the
+                            Ålesund area. Whether you're exploring the fjords, embarking on a scenic coastal drive, or
+                            simply navigating the charming streets of Ålesund itself, Rental Roulette ensures that your
+                            local car rental experience is not only convenient but also intimately connected to the unique
+                            beauty of this Norwegian gem. Embrace the essence of Ålesund with us, where every rental is a
+                            seamless blend of convenience and local charm.
+                        </p>
+                    </div>
+                </div>
             </section>
             <Footer />
         </div>
