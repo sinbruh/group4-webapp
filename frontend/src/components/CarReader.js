6238790f6d19999370cae59191877a44febac17e
Leander-Haddal <122444622+Leander-Haddal@users.noreply.github.com>
2024-05-21 20:50:54 +0200

    Update to CarReader
    rentals and providers

--- frontend/src/components/CarReader.js
+++ frontend/src/components/CarReader.js
@@ -1,123 +1,145 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import CarCard from "@/components/CarCard";
 import ExpandedCard from "@/components/ExpandedCard";
 
-export default function CarReader({ location, dates, price, setExpandedCarInfo }) {
-    const [cars, setCars] = useState([]);
-    const [expandedCar, setExpandedCar] = useState(null);
-    const fromDate = dates ? dates.from : null;
-    const toDate = dates ? dates.to : null;
+export default function CarReader({
+  location,
+  dates,
+  price,
+  setExpandedCarInfo,
+}) {
+  const [cars, setCars] = useState([]);
+  const [expandedCar, setExpandedCar] = useState(null);
+  const fromDate = dates ? dates.from : null;
+  const toDate = dates ? dates.to : null;
 
-    const updateJsonFile = async () => {
-        try {
+  const updateJsonFile = async () => {
+    try {
+      //check data
+      const response = await fetch("http://localhost:8080/api/cars");
+       
 
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
 
-            //check data
-            const response = await fetch('http://localhost:8080/api/cars');
-            if (!response.ok) {
-                throw new Error(`HTTP error! status: ${response.status}`);
+      let data = await response.json();
+
+      if (data) {
+        // Add img property to each car configuration
+        data = data.map((item) => {
+          item.configurations = item.configurations.map((config) => {
+            config.img = `${item.make.replace(/ /g, "-")}-${item.model.replace(
+              / /g,
+              "-"
+            )}.webp`;
+            return config;
+          });
+          return item;
+        });
+      }
+
+      //update CarCards
+      setCars(data);
+    } catch (error) {
+      console.error("Error updating JSON file:", error);
+    }
+  };
+
+  useEffect(() => {
+    updateJsonFile().catch(console.error);
+  }, [location, dates, price]);
+
+  
+
+  const handleCardClick = (car) => {
+    setExpandedCar(car);
+  };
+
+  console.log("CarReaderStage1", cars);
+
+  return (
+    <div>
+      <div id="cards">
+        {cars
+          .flatMap((car) =>
+            car.configurations[0].providers.map((provider) => ({
+              ...car,
+              provider,
+            }))
+          )
+          .filter(({ provider }) => {
+            if (
+              typeof price === "undefined" ||
+              typeof location === "undefined"
+            ) {
+              return true;
             }
 
-            let data = await response.json();
+            const lowestPrice = provider.price;
+            const fromPriceNumber = Number(price.min);
+            const toPriceNumber = Number(price.max);
+            const carLocationLowercase = provider.location.toLowerCase();
+            const locationLowercase = location.toLowerCase();
+            const carAvailability = provider.available;
 
-            if (data) {
+            if (!isNaN(fromPriceNumber) && !isNaN(toPriceNumber)) {
+              const isFromPriceLower = fromPriceNumber <= lowestPrice;
+              const isToPriceHigher =
+                toPriceNumber >= lowestPrice || toPriceNumber === 0;
+              const isLocationMatch =
+                carLocationLowercase === locationLowercase || location === "";
+              if (isFromPriceLower && isToPriceHigher && isLocationMatch) {
+                return lowestPrice;
+              }
+            }
+            return false;
+          })
+          .map(({ configurations, provider, make, model }) => {
+            const carImageName = configurations[0].img || "default.jpg";
 
+            console.log("CarReaderStage2", configurations[0]);
 
-            // Add img property to each car configuration
-            data = data.map(item => {
-                item.configurations = item.configurations.map(config => {
-                    config.img = `${item.make.replace(/ /g, '-')}-${item.model.replace(/ /g, '-')}.webp`;
-                    return config;
-                });
-                return item;
-            });
-        }
+            const allRentals = configurations.flatMap((configuration) =>
+              configuration.providers.flatMap((provider) => provider.rentals)
+            );
 
-            //update CarCards
-            setCars(data);
-        } catch (error) {
-            console.error('Error updating JSON file:', error);
-        }
-    };
+            const providerRentals = provider.rentals || [];
 
-        useEffect(() => {
-
-
-            updateJsonFile().catch(console.error);
-
-        }, [location, dates, price]);
-
-    const handleCardClick = (car) => {
-        setExpandedCar(car);
-    };
-
-    return (
-        <div>
-            <div id="cards">
-                {cars
-                    .flatMap(car => car.configurations[0].providers.map(provider => ({...car, provider})))
-                    .filter(({provider}) => {
-                        if (typeof price === 'undefined' || typeof location === 'undefined') {
-                            return true;
-                        }
-
-                        const lowestPrice = provider.price;
-                        const fromPriceNumber = Number(price.min);
-                        const toPriceNumber = Number(price.max);
-                        const carLocationLowercase = provider.location.toLowerCase();
-                        const locationLowercase = location.toLowerCase();
-                        const carAvailability = provider.available;
-
-
-
-                        if (!isNaN(fromPriceNumber) && !isNaN(toPriceNumber)) {
-                            const isFromPriceLower = fromPriceNumber <= lowestPrice;
-                            const isToPriceHigher = toPriceNumber >= lowestPrice || toPriceNumber === 0;
-                            const isLocationMatch = carLocationLowercase  === locationLowercase || location === '';
-                            if (isFromPriceLower && isToPriceHigher && isLocationMatch) {
-                                return lowestPrice;
-                            }
-                        }
-                        return false;
-                    })
-                    .map(({configurations, provider, make, model}) => {
-                        const carImageName = configurations[0].img || 'default.jpg';
-
-
-                        const rentals = configurations[0].providers[0].rentals;
-                        const isAvailable = rentals ? rentals.every(rental => {
-                            const rentalStartDate = new Date(rental.startDate);
-                            const rentalEndDate = new Date(rental.endDate);
-                            return rentalStartDate > toDate || rentalEndDate < fromDate;
-                        }): false;
-
-                        const carInfo = {
-                            configId : configurations[0].id,
-                            carImageInput : carImageName,
-                            carName : `${make} ${model}`,
-                            price : provider.price,
-                            location : provider.location || 'undefined',
-                            size : configurations[0].numberOfSeats,
-                            fuelType : configurations[0].fuelType,
-                            transmission : configurations[0].tranmissionType,
-                            availability : isAvailable,
-                            provider : [provider],
-                            configurations: configurations
-                        }
-
-
-
-                        return (
-                        <CarCard key={provider.id} carInfo={carInfo} setExpandedCarInfo={setExpandedCarInfo}/>
-                    );
+            const isAvailable = providerRentals
+              ? providerRentals.every((rental) => {
+                  const rentalStartDate = new Date(rental.startDate);
+                  const rentalEndDate = new Date(rental.endDate);
+                  return rentalStartDate > toDate || rentalEndDate < fromDate;
                 })
-            }
-            </div>
+              : false;
 
-            {expandedCar &&
-            <ExpandedCard carInfo={carInfo}/>
-            }
-        </div>
-    );
-};
+            const carInfo = {
+              configId: configurations[0].id,
+              carImageInput: carImageName,
+              carName: `${make} ${model}`,
+              price: provider.price,
+              location: provider.location|| 'undefined',
+              size: configurations[0].numberOfSeats,
+              fuelType: configurations[0].fuelType,
+              transmission: configurations[0].tranmissionType,
+              availability: isAvailable,
+              provider: [provider],
+              configurations: configurations,
+              rentals: providerRentals,
+            };
 
+            return (
+              <CarCard
+                key={provider.id}
+                carInfo={carInfo}
+                setExpandedCarInfo={setExpandedCarInfo}
+              />
+            );
+          })}
+      </div>
+
+      {expandedCar && <ExpandedCard carInfo={carInfo} />}
+    </div>
+  );
+}
