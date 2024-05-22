0ec29b4045fabba6a266b878c894c8c6eabea51b
sinbruh <41494471+sinbruh@users.noreply.github.com>
2024-05-22 22:37:25 +0200

    update the backend to print pid

--- backend/src/main/java/no/ntnu/project/group4/webapp/WebappApplication.java
+++ backend/src/main/java/no/ntnu/project/group4/webapp/WebappApplication.java
@@ -2,11 +2,14 @@ package no.ntnu.project.group4.webapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 
 @SpringBootApplication
 public class  WebappApplication {
 
 	public static void main(String[] args) {
+		SpringApplication springApplication = new SpringApplication(WebappApplication.class);
+		springApplication.addListeners(new ApplicationPidFileWriter("webapp.pid"));
 		SpringApplication.run(WebappApplication.class, args);
 	}
 
