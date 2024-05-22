6138fad60137e88d489daa920f68d0da25ebf0e7
mathi <mathiaswestergaardjohansen@gmail.com>
2024-05-22 13:48:41 +0200

    added image background for contact page, styled it accordingly

--- frontend/src/app/contact/page.js
+++ frontend/src/app/contact/page.js
@@ -1,32 +1,50 @@
 import React from 'react';
 import Navigation from "@/components/Navigation";
 import { Footer } from "@/components/Footer";
-import Link from "next/link";
+import Image from "next/image";
+import contact from "/img/contact.png";
 
 export default function Page() {
     return (
-        <div className="bg-[url('/temp-background-image-low.webp')] bg-cover bg-center min-h-screen flex flex-col justify-between">
+        <div className="min-h-screen flex flex-col justify-between bg-gray-100">
             <Navigation />
-            <section className="text-gray-100 body-font">
-                <div className="container px-5 py-24 mx-auto flex flex-col items-center">
-                    <div className="w-full lg:w-2/3 bg-white bg-opacity-80 rounded-lg p-8 shadow-lg">
-                        <h1 className="title-font font-semibold text-4xl text-gray-900 mb-4">Contact Us</h1>
-                        <p className="leading-relaxed mt-4 text-lg bg-opacity-50 p-6 rounded-lg text-gray-900 mb-6">
-                            Have a question, concern, or feedback? We'd love to hear from you! Feel free to reach out to us using the contact information provided below, and we'll get back to you as soon as possible.
-                        </p>
-                        <div className="mb-6">
+            <div className="flex-grow"></div>
+            <div className="relative flex justify-center items-center mb-8">
+                <Image src={contact} alt="Contact" width={1550} height={80} className="w-full h-auto" />
+                <div className="absolute inset-0 flex justify-center items-center">
+                    <h1 className="title-font font-semibold text-6xl text-white">Contact Us</h1>
+                </div>
+            </div>
+            <section className="text-gray-900 body-font w-full py-12 mt-auto">
+                <div className="container px-5 mx-auto flex flex-col items-center">
+                    <div className="w-full flex flex-wrap justify-center">
+                        <div className="flex flex-col items-start mb-6 w-full md:w-1/3 px-4">
+                            <p className="leading-relaxed text-lg text-gray-900 mb-6">
+                                Have a question, concern, or feedback? We'd love to hear from you!
+                            </p>
+                            <p className="leading-relaxed text-lg text-gray-900 mb-6">
+                                Feel free to reach out to us using the contact information provided below, and we'll get back to you as soon as possible.
+                            </p>
+                        </div>
+                        <div className="flex flex-col items-start mb-6 w-full md:w-1/3 px-4">
                             <h2 className="text-gray-900 text-xl font-medium title-font mb-3">Technical Support</h2>
                             <p className="leading-relaxed text-base text-gray-900">
-                                In case of any technical issues, please contact our support team at <span className="font-semibold">999 99 999</span>. Our team is available 24/7 to assist you with any troubleshooting or inquiries if you encounter any difficulties while using our platform.
+                                In case of any technical issues, please contact our support team at <span className="font-semibold">999 99 999</span>.
+                            </p>
+                            <p className="leading-relaxed text-base text-gray-900">
+                                Our team is available 24/7 to assist you with any troubleshooting or inquiries if you encounter any difficulties while using our platform.
                             </p>
                         </div>
-                        <div className="mb-6">
+                        <div className="flex flex-col items-start mb-6 w-full md:w-1/3 px-4">
                             <h3 className="text-gray-900 text-xl font-medium title-font mb-3">Media Inquiries</h3>
                             <p className="leading-relaxed text-base text-gray-900">
-                                For media inquiries, please contact our press team at <span className="font-semibold">999 99 999</span>. Our team is available Monday through Friday, from 9:00 AM to 5:00 PM.
+                                For media inquiries, please contact our press team at <span className="font-semibold">999 99 999</span>.
+                            </p>
+                            <p className="leading-relaxed text-base text-gray-900">
+                                Our team is available Monday through Friday, from 9:00 AM to 5:00 PM.
                             </p>
                         </div>
-                        <div className="mb-6">
+                        <div className="flex flex-col items-start mb-6 w-full md:w-1/3 px-4">
                             <h3 className="text-gray-900 text-xl font-medium title-font mb-3">Address</h3>
                             <p className="leading-relaxed text-base text-gray-900">
                                 Vestre Olsvikveg 13, 6019 Ålesund, Norway
@@ -38,4 +56,4 @@ export default function Page() {
             <Footer />
         </div>
     );
-};
+}
